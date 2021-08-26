212911c68fee1c1e656cfffe4365b0d5fce1d75c
Saegl <saegl@protonmail.com>
2021-08-26 14:35:18 +0600

    tf.Dataset is now used
    Former-commit-id: 305d85045049731112cf77140d089c150d540c79

---
+++ dinora/net.py
@@ -0,0 +1,60 @@
+from os.path import dirname, realpath, join
+
+import chess
+from tensorflow import keras
+import numpy as np
+
+from .board_utils import canon_input_planes
+from .policy import move_lookup, flipped_move_lookup
+
+
+MODELS_DIR = join(dirname(realpath(__file__)), '..', 'models')
+BEST_MODEL = join(MODELS_DIR, 'best_model.h5')
+
+
+class ChessModel:
+    def __init__(self, model_path=BEST_MODEL) -> None:
+        self.model: keras.Model = keras.models.load_model(model_path)
+
+    def model_out(self, board: chess.Board):
+        flip = board.turn
+        plane = canon_input_planes(board.fen(), flip)
+        plane = np.array([plane])
+        model_output = self.model(plane, training=False)
+        return model_output
+
+    def raw_eval(self, board: chess.Board):
+        policy, value = self.model_out(board)
+        # unwrap tensor
+        policy = policy[0]
+        value = float(value[0])
+        value = value if board.turn else -value
+
+        # take only legal moves from policy
+        moves = []
+        policies = []
+        lookup = move_lookup if board.turn else flipped_move_lookup
+        for move in board.legal_moves:
+            i = lookup[move]
+            moves.append(move.uci())
+            policies.append(float(policy[i]))
+
+        # map to sum(policies) == 1
+        s = sum(policies)
+        policies = map(lambda e: e / s, policies)
+
+        return dict(zip(moves, policies)), value
+
+    def evaluate(self, board: chess.Board):
+        result = None
+        if board.is_game_over(claim_draw=True):
+            result = board.result(claim_draw=True)
+
+        if result != None:
+            if result == "1/2-1/2":
+                return dict(), 0.0
+            else:
+                # Always return -1.0 when checkmated
+                # and we are checkmated because it's our turn to move
+                return dict(), -1.0
+        return self.raw_eval(board)
