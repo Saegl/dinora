88573b8d90310ad3a091dab149bb6f4e3e910241
Saegl <saegl@protonmail.com>
2021-09-09 23:28:05 +0600

    Add selfplay dataset

--- dinora/dataset.py
+++ dinora/dataset.py
@@ -1,18 +1,30 @@
 import tensorflow as tf
 
 from .preprocess_pgn import load_chess_games, chess_positions
+from .selfplay import gen_game
+
+default_dinora_signature = (
+    (
+        tf.TensorSpec(shape=(18, 8, 8), dtype=tf.float32, name=None),  # Board
+        (
+            tf.TensorSpec(shape=(1968,), dtype=tf.float32, name=None),  # Policy
+            tf.TensorSpec(shape=(), dtype=tf.float32, name=None),  # Game result
+        ),
+    ),
+)
 
 
 def create_dataset_from_pgn(pgn_path: str, max_games: int) -> tf.data.Dataset:
     games = load_chess_games(pgn_path, max_games)
     tfdataset = tf.data.Dataset.from_generator(
-        lambda: chess_positions(games),
-        output_signature=(
-            tf.TensorSpec(shape=(18, 8, 8), dtype=tf.float32, name=None),
-            (
-                tf.TensorSpec(shape=(1968,), dtype=tf.float32, name=None),
-                tf.TensorSpec(shape=(), dtype=tf.float32, name=None),
-            ),
-        ),
+        lambda: chess_positions(games), output_signature=default_dinora_signature
+    )
+    return tfdataset
+
+
+def create_dataset_from_selfplay(nodes, net, c) -> tf.data.Dataset:
+    games = [gen_game(nodes, net, c)]
+    tfdataset = tf.data.Dataset.from_generator(
+        lambda: chess_positions(games), output_signature=default_dinora_signature
     )
     return tfdataset
