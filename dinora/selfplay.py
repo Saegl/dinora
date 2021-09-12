eeb15d95f22529928f8f0954e7a50b377ff6145f
Saegl <saegl@protonmail.com>
2021-09-12 16:23:33 +0600

    Update Selfplay
    Former-commit-id: 3f472839ba15d9c4926c9c2408f6ab48e3762c9a

--- dinora/selfplay.py
+++ dinora/selfplay.py
@@ -2,28 +2,40 @@ import chess
 from chess.pgn import Game
 
 from .utils import disable_tensorflow_log
+
 disable_tensorflow_log()
 from .search import uct_nodes
 
-def gen_game(nodes: int, net, c):
+
+def gen_game(nodes: int, net, c_puct, dirichlet_alpha, noise_eps, softmax_temp):
     game = Game()
     node = game
     while not node.board().is_game_over(claim_draw=True):
-        move, _ = uct_nodes(node.board(), nodes, net, c)
+        move, _ = uct_nodes(
+            node.board(),
+            nodes,
+            net,
+            c_puct,
+            None,
+            dirichlet_alpha,
+            noise_eps,
+            softmax_temp,
+        )
         move = chess.Move.from_uci(move)
         node = node.add_variation(move)
-        print(move, end=' ', flush=True)
+        print(move, end=" ", flush=True)
     print(node.board().result(claim_draw=True))
 
     result = node.board().result(claim_draw=True)
-    game.headers['Result'] = result
+    game.headers["Result"] = result
     return game
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from .net import ChessModel
+
     nodes = 10
-    net = ChessModel('models/best_light_model.h5')
+    net = ChessModel("models/best_light_model.h5")
     c = 2.0
     game = gen_game(nodes, net, c)
     print(game)
--- dinora/training.py
+++ dinora/training.py
@@ -1,4 +1,5 @@
 from dinora.utils import disable_tensorflow_log
+
 disable_tensorflow_log()
 from tensorflow import keras
 
@@ -8,28 +9,37 @@ from dinora.model import build_model, LightConfig
 from dinora.selfplay import gen_game
 
 nodes = 5
-net = ChessModel('models/best_light_model.h5')
+net = ChessModel("models/best_light_model.h5")
 net.model = build_model(LightConfig)  # Start from zero
 net.model.compile(
     keras.optimizers.Adam(0.05),
     loss=["categorical_crossentropy", "mean_squared_error"],
     metrics=["accuracy"],
 )
-c = 2.0
+
+c_puct = 2.0
+softmax_temp = 1.6
+dirichlet_alpha = 0.3
+noise_eps = 0.2
 
 stats = {}
 i = 0
 while True:
-    game = gen_game(nodes, net, c) # Selfplay game 
-    games = [game]
-    dataset = create_dataset_from_games(games)
+    selfplay_games = [
+        gen_game(nodes, net, c_puct, dirichlet_alpha, noise_eps, softmax_temp)
+        for _ in range(3)
+    ]
+
+    dataset = create_dataset_from_games(selfplay_games)
     dataset = dataset.batch(4096)
     net.model.fit(dataset, epochs=1)
-    
-    res = game.headers['Result']
-    stats[res] = stats.setdefault(res, 0) + 1
-    
+
+    for g in selfplay_games:
+        res = g.headers["Result"]
+        stats[res] = stats.setdefault(res, 0) + 1
+
     if i % 256 == 0:
-        net.model.save_weights('weights.h5')
+        net.model.save_weights("weights.h5")
         print(stats)
+        print(selfplay_games[0])
     i += 1
