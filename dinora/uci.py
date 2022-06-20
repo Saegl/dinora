e99e067e2385e6d9f936e1c647dff98c5df28d1f
Saegl <saegl@protonmail.com>
2022-06-21 04:19:48 +0600

    Fix uci state

--- dinora/uci.py
+++ dinora/uci.py
@@ -18,15 +18,22 @@ dirichlet_alpha = 0.3
 noise_eps = 0.00
 
 
-class UciState:
-    def __init__(self):
-        self.net = None
-
-
 def send(s: str):
     sys.stdout.write(s)
     sys.stdout.write("\n")
     sys.stdout.flush()
+
+class UciState:
+    def __init__(self):
+        self.net = None
+        self.board = chess.Board()
+    
+    def load_neural_network(self):
+        if self.net is None:
+            send("info string loading nn... it make take a while")
+            import dinora.net
+            self.net = dinora.net.ChessModel("models/latest.h5")
+            send("info string nn is loaded")
 
 
 def time_manager(moves_number: int, time_left: int, inc: int = 0) -> float:
@@ -36,42 +43,41 @@ def time_manager(moves_number: int, time_left: int, inc: int = 0) -> float:
     return move_time
 
 
-def uci_command(state: UciState, cmd: str, board: chess.Board):
+def uci_command(state: UciState, cmd: str):
     tokens = cmd.split()
     if tokens[0] == "uci":
-        send("id name Dinora")
+        send("id name Dinora Docker")
         send("id author Saegl")
         send("option name model type string default models/latest.h5")
         send("uciok")
     elif tokens[0] == "isready":
-        import dinora.net
-
-        if state.net is None:
-            state.net = dinora.net.ChessModel("models/latest.h5")
+        state.load_neural_network()
         send("readyok")
     elif tokens[0] == "ucinewgame":
         pass
     elif tokens[0] == "position":
         if tokens[1] == "startpos":
-            board = chess.Board()
+            state.board = chess.Board()
         if tokens[1] == "fen":
             fen = " ".join(tokens[2:8])
-            board = chess.Board(fen)
+            state.board = chess.Board(fen)
 
         if "moves" in tokens:
             index = tokens.index("moves")
             moves = tokens[index + 1 :]
             for move in moves:
-                board.push_uci(move)
+                send(f"info string pushing move {move}")
+                state.board.push_uci(move)
     elif tokens[0] == "go":
+        state.load_neural_network()
         # go wtime <wtime> btime <btime>
         if len(tokens) == 5 and tokens[1] == "wtime":
             wtime = int(tokens[2])
             btime = int(tokens[4])
-            engine_time = wtime if board.turn else btime
-            move_time = time_manager(board.fullmove_number, engine_time)
+            engine_time = wtime if state.board.turn else btime
+            move_time = time_manager(state.board.fullmove_number, engine_time)
             move, _ = search.uct_time(
-                board,
+                state.board,
                 state.net,
                 c_puct,
                 move_time,
@@ -86,11 +92,11 @@ def uci_command(state: UciState, cmd: str, board: chess.Board):
             btime = int(tokens[4])
             winc = int(tokens[6])
             binc = int(tokens[8])
-            engine_time = wtime if board.turn else btime
-            engine_inc = winc if board.turn else binc
-            move_time = time_manager(board.fullmove_number, engine_time, engine_inc)
+            engine_time = wtime if state.board.turn else btime
+            engine_inc = winc if state.board.turn else binc
+            move_time = time_manager(state.board.fullmove_number, engine_time, engine_inc)
             move, _ = search.uct_time(
-                board,
+                state.board,
                 state.net,
                 c_puct,
                 move_time,
@@ -101,7 +107,7 @@ def uci_command(state: UciState, cmd: str, board: chess.Board):
             )
         else:
             move, _ = search.uct_nodes(
-                board,
+                state.board,
                 300,
                 state.net,
                 c_puct,
@@ -119,10 +125,10 @@ def uci_command(state: UciState, cmd: str, board: chess.Board):
 
 def start_uci():
     uci_state = UciState()
-    board = chess.Board()
 
     while True:
-        uci_command(uci_state, input(), board)
+        line = input()
+        uci_command(uci_state, line)
 
 
 if __name__ == "__main__":
