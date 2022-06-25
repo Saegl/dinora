21c268f357e9532a89f3f6281cbed5ddf2577e42
Saegl <saegl@protonmail.com>
2022-06-26 02:01:20 +0600

    benchmarking
    Former-commit-id: 7bda89ac839bf2c5c59f527b676b8d8ddad27d5c

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -9,6 +9,8 @@ Welcome to Dinora's documentation!
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+   
+   benchmarking
 
 
 
---
+++ tests/bench/test_board_repr.py
@@ -0,0 +1,25 @@
+import json
+import pytest
+import chess
+import numpy as np
+
+from dinora.board_representation.canon_planes import canon_input_planes
+
+with open("tests/data/positions/various.json") as f:
+    various = json.load(f)
+
+canon_planes = np.load("tests/data/positions/canon_planes.npy")
+
+
+@pytest.mark.parametrize("index, pos", zip(range(len(various)), various))
+def test_bench_canon_input_planes(index, pos, benchmark):
+    """
+    This test checks the speed of converting board into planes
+    If you are willing to update canon_input_planes representation,
+    look at bin/update_canon_tests
+    """
+    board = chess.Board(pos)
+    planes = benchmark(canon_input_planes, pos, not board.turn)
+
+    assert planes.shape == (18, 8, 8)
+    assert np.allclose(planes, canon_planes[index])
