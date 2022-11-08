667ba9c27ddf78fab473bfb2340e2ac5b2f83886
Saegl <saegl@protonmail.com>
2022-11-08 10:42:23 +0600

    Update uci_info test

--- tests/unit/test_search.py
+++ tests/unit/test_search.py
@@ -1,11 +1,11 @@
-from dinora import search
+from dinora.mcts.uci_info import cp
 
 
 def test_cp():
     # Dead draw
-    assert search.cp(0.0) == 0
+    assert cp(0.0) == 0
 
     # q = 1.0 absolute win
     # q = -1.0 absolute lose
     # and they opposite to each other
-    assert search.cp(1.0) == -search.cp(-1.0)
+    assert cp(1.0) == -cp(-1.0)
