4bd31b1ace8530a006406eebef1c9de50de8697d
Saegl <saegl@protonmail.com>
2022-11-11 15:07:01 +0600

    Add badgyal model

---
+++ dinora/models/badgyal.py
@@ -0,0 +1,30 @@
+import chess
+from badgyal import BGNet
+
+from dinora.models import Priors, StateValue, BaseModel
+
+
+class BadgyalModel(BaseModel):
+    def __init__(self, softmax_temp: float) -> None:
+        self.softmax_temp: float = softmax_temp
+        self.badg = BGNet(False, False)
+
+    def evaluate(self, board: chess.Board) -> tuple[Priors, StateValue]:
+        result = board.result(claim_draw=True)
+        if result == "*":
+            # Game is not ended
+            # evaluate by using ANN
+            priors, value_estimate = self.badg.eval(board, self.softmax_temp)
+        elif result == "1/2-1/2":
+            # It's already draw
+            # or we can claim draw, anyway `value_estimate` is 0.0
+            priors = {}
+            value_estimate = 0.0
+        else:
+            # result == '1-0' or result == '0-1'
+            # we are checkmated because it's our turn to move
+            # so the `value_estimate` is -1.0
+            priors = {}  # no moves after checkmate
+            value_estimate = -1.0
+        priors = {chess.Move.from_uci(m): val for m, val in priors.items()}
+        return priors, value_estimate
