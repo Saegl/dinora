6e96b7c48ebe27e5baf8aec0d05b5db216eb2a78
Alisher Zhubanyshev <saegl@protonmail.com>
2023-07-12 23:59:05 +0600

    Improve boards evaluator callback

--- dinora/handmade_val_dataset/add_stockfish_eval.py
+++ dinora/handmade_val_dataset/add_stockfish_eval.py
@@ -1,26 +1,28 @@
+import pprint
 import chess
 import chess.engine
 from dinora.handmade_val_dataset.dataset import POSITIONS
 
 
-engine = chess.engine.SimpleEngine.popen_uci('stockfish')
+engine = chess.engine.SimpleEngine.popen_uci("stockfish")
 
 
 for pos in POSITIONS:
-    board = chess.Board(fen=pos['fen'])
-    text = pos['text']
+    board = chess.Board(fen=pos["fen"])
+    text = pos["text"]
 
-    info = engine.analyse(board, limit=chess.engine.Limit(nodes=100_000))
-    score = info['score']
-    pos['stockfish_cp'] = score.white().score(mate_score=10_000) / 100
+    info = engine.analyse(board, limit=chess.engine.Limit(nodes=100_000), multipv=3)
+    # pprint.pprint(info)
+    score = info[0]["score"]
+    pos["stockfish_cp"] = score.white().score(mate_score=10_000) / 100
     wdl = score.wdl(ply=board.ply()).pov(board.turn)
     win = wdl.winning_chance()
     draw = wdl.drawing_chance()
     loss = wdl.losing_chance()
-    pos['stockfish_wdl'] = f"w {win:.2f} | d {draw:.2f} | l {loss:.2f}"
-
+    pos["stockfish_wdl"] = f"{win:.2f} | {draw:.2f} | {loss:.2f}"
+    pos["stockfish_top3_lines"] = "\n".join([
+        f'{[e.uci() for e in line["pv"][:3]]}' for line in info[:3]
+    ])
 
 engine.quit()
-
-import pprint
 pprint.pprint(POSITIONS)
--- dinora/train_callbacks.py
+++ dinora/train_callbacks.py
@@ -9,35 +9,7 @@ import lightning.pytorch as pl
 from lightning.pytorch.callbacks import Callback
 
 from dinora.models.torchnet.resnet import ResNetLight
-
-
-POSITIONS = [
-    {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-        'material': '0',
-        'text': 'Starting chess position'
-    },
-    {
-        'fen': "4qn1k/2R3pp/p4p2/3Q4/P4N2/6P1/5PBP/2R3K1 w - - 0 31",
-        'material': '+13',
-        'text': 'White is clearly winning, mate in 6',
-    },
-    {
-        'fen': '4qn1k/p1R3pp/5p2/3Q4/P4N2/6P1/5PBP/2R3K1 b - - 0 30',
-        'material': '+13',
-        'text': 'Black is clearly losing, mate in 6',
-    },
-    {
-        'fen': 'r1r1q2k/5bpp/5p2/p3n3/P7/6P1/3Q1P1P/2R3K1 w - - 0 31',
-        'material': '-11',
-        'text': 'White is clearly losing, mate in 10',
-    },
-    {
-        'fen': 'r1r1q2k/p4bpp/5p2/4n3/P7/6P1/3Q1P1P/2R3K1 b - - 0 30',
-        'material': '-11',
-        'text': 'Black is clearly winning, mate in 7'
-    }
-]
+from dinora.handmade_val_dataset.dataset import POSITIONS
 
 
 class SampleGameGenerator(Callback):
@@ -55,8 +27,9 @@ class SampleGameGenerator(Callback):
 
 
 class BoardsEvaluator(Callback):
-    def __init__(self):
+    def __init__(self, render_image = False):
         self.positions = POSITIONS
+        self.render_image = render_image
     
     @staticmethod
     def board_to_image(board):
@@ -68,27 +41,26 @@ class BoardsEvaluator(Callback):
     
     def on_validation_end(self, trainer: pl.Trainer, pl_module: ResNetLight) -> None:
         data = []
-        COLUMNS = [
-            'board', 'fen', 'material',
-            'text', 'win_prob', 'draw_prob',
-            'lose_prob', 'bestmove'
+        COLUMNS = ['image'] * self.render_image + [
+            'fen', 'text', 'type',
+            'stockfish_cp', 'stockfish_wdl', 'stockfish_top3_lines',
+            'model_v', 'model_wdl', 'bestmove'
         ]
 
         for position in self.positions:
             board = chess.Board(fen=position['fen'])
-            wandb_image = wandb.Image(self.board_to_image(board))
-            policy, probs = pl_module.eval_by_network(board)
+            policy, probs = pl_module.eval_by_network(board)  # TODO: eval in batch
+            model_wdl = f"{probs[0]:.2f} | {probs[1]:.2f} | {probs[2]:.2f}"
             bestmove = max(policy, key=lambda k: policy[k])
 
-            data.append([
-                wandb_image,
-                position['fen'],
-                position['material'],
-                position['text'],
-                probs[0],
-                probs[1],
-                probs[2],
-                bestmove.uci(),
-            ])
+            entry = [
+                position['fen'], position['text'], position['type'],
+                position['stockfish_cp'], position['stockfish_wdl'], position['stockfish_top3_lines'],
+                probs[0] - probs[2], model_wdl, bestmove.uci(),
+            ]
+            if self.render_image:
+                entry = [wandb.Image(self.board_to_image(board))] + entry
+            
+            data.append(entry)
         
         trainer.logger.log_text(key='val_positions', columns=COLUMNS, data=data)
