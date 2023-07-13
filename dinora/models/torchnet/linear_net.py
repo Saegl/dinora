8e375ca9d4133728fb4235dcfbd15413cd199df8
Alisher Zhubanyshev <saegl@protonmail.com>
2023-07-14 00:20:44 +0600

    Convert LinearNN to LightningModule

--- dinora/models/torchnet/linear_net.py
+++ dinora/models/torchnet/linear_net.py
@@ -1,7 +1,10 @@
-from torch import nn
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+import lightning.pytorch as pl
 
-class LinearNN(nn.Module):
+class LinearNN(pl.LightningModule):
     def __init__(self) -> None:
         super().__init__()
         self.flatten = nn.Flatten()
@@ -15,14 +18,33 @@ class LinearNN(nn.Module):
 
         self.policy_output = nn.Sequential(
             nn.Linear(2048, 1880),
-            nn.Softmax(dim=-1),
         )
         self.value_output = nn.Sequential(
-            nn.Linear(2048, 1),
-            nn.Sigmoid(),
+            nn.Linear(2048, 3),
         )
 
     def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return self.policy_output(logits), self.value_output(logits)
+
+    def training_step(self, batch, batch_idx):
+        x, (y_policy, y_value) = batch
+        y_hat_policy, y_hat_value = self(x)
+        
+        policy_loss = F.cross_entropy(y_hat_policy, y_policy)
+        value_loss = F.cross_entropy(y_hat_value, y_value)
+        cumulative_loss = policy_loss + value_loss
+
+        self.log_dict({
+            'train/policy_loss': policy_loss,
+            'train/value_loss': value_loss,
+            'train/cumulative_loss': cumulative_loss
+        })
+        
+        return cumulative_loss
+    
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
