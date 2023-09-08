397c5049164344ef9d27c8461a3b1ef6f14b38f2
Alisher Zhubanyshev <saegl@protonmail.com>
2023-09-08 22:38:23 +0600

    Fix relative dataset path

--- dinora/train/handmade_val_dataset/dataset.py
+++ dinora/train/handmade_val_dataset/dataset.py
@@ -1,5 +1,6 @@
 import json
+from dinora import PROJECT_ROOT
 
 
-with open("data/handmade_val/handmade.json", "rt", encoding="utf8") as f:
+with open(PROJECT_ROOT / "data/handmade_val/handmade.json", "rt", encoding="utf8") as f:
     POSITIONS = json.load(f)
