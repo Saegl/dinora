656b83ed892b2daa51741c8bc6aae2d0ca642139
Saegl <saegl@protonmail.com>
2025-02-20 17:30:03 +0500

    Fix onnx batch_size exporter

--- src/onnxtools/export_onnx.py
+++ src/onnxtools/export_onnx.py
@@ -21,4 +21,9 @@ def export_onnx(model_name: str, weights: pathlib.Path) -> None:
         do_constant_folding=True,
         input_names=["input"],
         output_names=["policy", "value"],
+        dynamic_axes={
+            "input": {0: "batch_size"},
+            "policy": {0: "batch_size"},
+            "value": {0: "batch_size"},
+        },
     )
