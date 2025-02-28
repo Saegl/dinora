b84c90f854089165fdad7faf79bd082854b4e591
Saegl <saegl@protonmail.com>
2025-02-28 17:55:15 +0500

    Add UCI test

---
+++ tests/test_uci.py
@@ -0,0 +1,14 @@
+import chess
+import chess.engine
+
+
+def test_uci():
+    uci = chess.engine.SimpleEngine.popen_uci(["python", "-m", "dinora"])
+
+    board = chess.Board()
+    play_res = uci.play(board, chess.engine.Limit(time=0.1))
+
+    assert play_res.move is not None
+    assert play_res.move.uci() in ["e2e4", "d2d4", "c2c4", "Nf3"]
+
+    uci.close()
