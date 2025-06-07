d7b9e3f6b610ab0cfc347ead128f59ec4ce2b1c8
Saegl <saegl@protonmail.com>
2025-06-07 20:25:33 +0500

    Test UCI with onnx model

--- tests/test_uci.py
+++ tests/test_uci.py
@@ -3,7 +3,9 @@ import chess.engine
 
 
 def test_uci():
-    uci = chess.engine.SimpleEngine.popen_uci(["python", "-m", "dinora"])
+    uci = chess.engine.SimpleEngine.popen_uci(
+        ["python", "-m", "dinora", "--model=onnx", "--device=cpu"]
+    )
 
     board = chess.Board()
     play_res = uci.play(board, chess.engine.Limit(time=0.1))
