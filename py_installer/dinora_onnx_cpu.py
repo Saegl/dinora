b1abb4a51145a4ac88482966bf50bf2c4ec1d5ae
Saegl <saegl@protonmail.com>
2025-09-06 17:54:59 +0500

    Set onnx threads

--- py_installer/dinora_onnx_cpu.py
+++ py_installer/dinora_onnx_cpu.py
@@ -1,3 +1,8 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+
 from dinora.uci.cli import DefaultArgs, main
 
 main(DefaultArgs(searcher="mcts", model="onnx", device="cpu"))
